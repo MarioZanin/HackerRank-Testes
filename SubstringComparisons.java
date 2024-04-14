51a6129a86ad272a9349a265848ba842b8f0a645
MarioZanin <84603609+MarioZanin@users.noreply.github.com>
2024-04-14 18:48:58 -0300

    Create SubstringComparisons.java
    Código Original escrito em JAVA

---
+++ SubstringComparisons.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+ public static String getSmallestAndLargest(String s, int k) {
+        String smallest = s.substring(0, k);
+        String largest = s.substring(0, k);
+
+        for (int i = 1; i <= s.length() - k; i++) {
+            String substr = s.substring(i, i + k);
+            if (substr.compareTo(smallest) < 0) {
+                smallest = substr;
+            }
+            if (substr.compareTo(largest) > 0) {
+                largest = substr;
+            }
+        }
+
+        return smallest + "\n" + largest;
+    }
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution.*/ 
+        
+    Scanner scanner = new Scanner(System.in);
+        String s = scanner.next();
+        int k = scanner.nextInt();
+        scanner.close();
+
+        String result = getSmallestAndLargest(s, k);
+        System.out.println(result);
+    
+    }
+}
